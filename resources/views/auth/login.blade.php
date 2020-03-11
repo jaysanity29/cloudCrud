cc86275a97dfca4437085f11fde4ace81d242a6b
jaysanity29 <subangjay@gmail.com>
2020-03-11 16:07:36 +0800

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,123 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-4">
+            <!-- <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div> -->
+            <div class="card mt-4">
+                <div class="view view-cascade gradient-card-header blue-gradient narrower py-4 mx-4 d-flex z-depth-3 justify-content-between align-items-center rounded text-center"
+                    style="margin-top:-25px;">
+                    <h2 class="text-center"><a href="#" class="white-text mx-3 text-center">Login</a></h2>
+                </div>
+               
+                <form class="text-center border border-light p-5" method="POST" action="{{ route('login') }}">
+                  @csrf
+                    <input id="email" type="email" class="form-control mb-4  @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="E-mail">
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                  
+                    <input type="password" id="defaultLoginFormPassword" class="form-control mb-4  @error('password') is-invalid @enderror"
+                        placeholder="Password" name="password" required autocomplete="current-password">
+
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                    <div class="d-flex justify-content-around">
+                        <div>
+                         
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember">
+                                <label class="custom-control-label" for="defaultLoginFormRemember">Remember me</label>
+                            </div>
+                        </div>
+                        <div>
+                        
+                            <a href="">Forgot password?</a>
+                        </div>
+                    </div>
+
+               
+                    <button class="btn btn-info btn-block my-4" type="submit">Sign in</button>
+
+                   
+                    <p>Not a member?
+                        <a href="">Register</a>
+                    </p>
+
+
+                </form>
+              
+            </div>
+
+        </div>
+    </div>
+</div>
+@endsection
